bbe34dbc4c60b9ff589b09bd69f2238bd18049c8
Nikolay <Onedaydejavu124@mail.ru>
2022-09-15 23:11:53 +0700

    // Написать программу преобразования десятичного числа в двоичное (решение через строки)

---
+++ EX003/Program.cs
@@ -0,0 +1,14 @@
+﻿// Написать программу преобразования десятичного числа в двоичное (решение через строки)
+
+Console.WriteLine("Введите чило: ");
+int num = int.Parse(Console.ReadLine() ??"0");
+
+string result = String.Empty;
+
+while (num > 0)
+{
+    result = Convert.ToString(num % 2) + result;
+    num = num / 2;
+}
+Console.WriteLine(result);
+
