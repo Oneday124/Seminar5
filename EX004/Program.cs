e0a6cd2cce03d0566a03c28ce0dd7f402fb41846
Nikolay <Onedaydejavu124@mail.ru>
2022-09-15 23:46:04 +0700

    Показать числа Фибоначи меньше заданного числа N

---
+++ EX004/Program.cs
@@ -0,0 +1,19 @@
+﻿// Показать числа Фибоначи меньше заданного числа N
+
+Console.WriteLine("Введите число: ");
+int N = int.Parse(Console.ReadLine() ??"0");
+int i = 0;
+int j = 1;
+int result = i + j;
+
+Console.WriteLine(i);
+Console.WriteLine(j);
+while(result + i < N)
+{
+    result = i + j;
+    i = j;
+    j = result;
+    Console.WriteLine(result);
+}
+
+
